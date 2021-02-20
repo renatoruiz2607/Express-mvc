dcc724bb571aa09c3e62f4ce88dfeecca95e760b
Renato Ruiz <renatoruiz2607@gmail.com>
2021-02-20 10:08:50 -0300

    sixth commit

--- routes/products.js
+++ routes/products.js
@@ -4,20 +4,38 @@ const model = require('../model/products')
 
 var router = express.Router();
 
+const products = [{
+  name: 'geladeira',
+  preco: 800,
+  type: 'eletronic'
+}, {
+  name: 'arroz',
+  price: 30,
+  type: 'food'
+}]
+
+const types = [{
+  id: "eletronic",
+  label: "Eletrônico"
+}, {
+  id: "food",
+  label: "Alimentício"
+}, {
+  id: "pharmaceutical",
+  label: "Farmacêutico"
+}];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const types = [{
-    id: "eletronic",
-    label: "Eletrônico"
-  }, {
-    id: "food",
-    label: "Alimentício"
-  }, {
-    id: "pharmaceutical",
-    label: "Farmacêutico"
-  }];
   res.render('products', { name: "Página de produtos", types: types });
+});
+
+router.post('/', function(req, res) {
+  res.render('products', { name: "Cadastro de Produtos", types: types });
+});
+
+router.get('/list', function(req, res) {
+  res.render('productsList', { products: products });
 });
 
 module.exports = router;
